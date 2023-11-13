cc017c8326befc072c741ac50cb441939692732a
ljt528 <ljt528@naver.com>
2023-11-14 05:01:10 +0900

    Refactor: "변경 출력 방법"

--- src/main/java/clipboard/OrderMenu.java
+++ src/main/java/clipboard/OrderMenu.java
@@ -1,9 +1,11 @@
 package clipboard;
 import ui.input.MenuInput;
 
+import java.util.Arrays;
+
 public class OrderMenu {
     public static void organizeorder() {
         String[] order = MenuInput.customerInputMenu();
-        System.out.println(order);
+        System.out.println(Arrays.toString(order));
     }
 }

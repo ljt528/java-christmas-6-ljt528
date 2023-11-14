57f5850760a70185f5ff88e2060583964dceaf09
ljt528 <ljt528@naver.com>
2023-11-14 20:45:55 +0900

    Feat: "추가 입력받은 메뉴 정리 기능"

--- src/main/java/clipboard/OrderMenu.java
+++ src/main/java/clipboard/OrderMenu.java
@@ -1,11 +1,27 @@
 package clipboard;
 import ui.input.MenuInput;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 
 public class OrderMenu {
-    public static void organizeOrder() {
+    public static void deliverOrder() {
         String[] order = MenuInput.customerInputMenu();
-        System.out.println(Arrays.toString(order));
+        organizeOrder(order);
+    }
+    public static void organizeOrder(String[] order) {
+        // 메뉴 확인
+        List<String> menuname = new ArrayList<>();
+        for (int i = 0; i < order.length; i++) {
+            menuname.add(i, (order[i].substring(0, order[i].indexOf("-"))));
+            System.out.println(menuname.get(i));
+        }
+
+        // 갯수 확인
+        List<Integer> menucount = new ArrayList<>();
+        for (int j = 0; j < order.length; j++) {
+            menucount.add(j, Integer.valueOf(order[j].substring(order[j].lastIndexOf("-") + 1)));
+            System.out.println(menucount.get(j));
+        }
     }
 }

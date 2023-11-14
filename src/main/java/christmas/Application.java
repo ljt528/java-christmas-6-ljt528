729c36020baf1a658f75340a1754dc7c797a572d
ljt528 <ljt528@naver.com>
2023-11-14 22:09:34 +0900

    Feat: "추가 주문 메뉴 출력 기능"

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -2,10 +2,16 @@ package christmas;
 
 import clipboard.OrderMenu;
 import ui.input.DateInput;
+import ui.output.OrderMenuOutput;
+
+import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
         DateInput.customerInputDate();
-        OrderMenu.organizeorder();
+        OrderMenu.deliverOrder();
+        List<String> menuNames = OrderMenu.getMenuNames();
+        List<Integer> menuCounts = OrderMenu.getMenuCounts();
+        OrderMenuOutput.printMenu(menuNames, menuCounts);
     }
 }

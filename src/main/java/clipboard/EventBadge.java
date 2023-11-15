0005a71b46c5ec79f34191bb42641734c895e006
ljt528 <ljt528@naver.com>
2023-11-15 20:28:15 +0900

    Feat: "추가 이벤트 배지 기능"

---
+++ src/main/java/clipboard/EventBadge.java
@@ -0,0 +1,13 @@
+package clipboard;
+
+public class EventBadge {
+//    public void Badge() {
+//        if (totalbenefitamount >= 20000) {
+//            starbadge
+//        } else if (totalbenefitamount >= 10000) {
+//            treebadge
+//        } else if (totalbenefitamount >= 5000) {
+//            santabadge
+//        }
+//    }
+}
